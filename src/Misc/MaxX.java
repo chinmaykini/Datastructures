860d2645e48032d46568b0dc1c591f621b7f9792
praful1984 <prafulla.tcs@gmail.com>
2016-08-05 19:34:16 -0700

    Fixed for none case by settting max=0

--- src/Misc/MaxX.java
+++ src/Misc/MaxX.java
@@ -9,7 +9,7 @@ public class MaxX {
 	private static int maxx(int[] arr) {
 		PriorityQueue<Integer> queue = new PriorityQueue<Integer>();
 		int lastIndex=1;
-		int max=Integer.MIN_VALUE;
+		int max=0;
 		for( int i=0; i < arr.length ; i++){
 			if( arr[i] >= lastIndex ){
 				int toBeRemovedVal= 0;
