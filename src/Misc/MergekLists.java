f424f16a89d0c2294bb12ca6c0ce3d12f0ed5bd6
praful1984 <prafulla.tcs@gmail.com>
2016-08-06 13:56:16 -0700

    Merge K sorted list into one sorted list

---
+++ src/Misc/MergekLists.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class MergeKLists {
+
+	static class ListNode {
+		public int val;
+		public ListNode next;
+		ListNode(int x) { val = x; next = null; }
+	}
+
+	public ListNode mergeKLists(ArrayList<ListNode> A) {
+		Comparator<ListNode> comp = new Comparator<ListNode>() {
+			@Override
+			public int compare(ListNode n1, ListNode n2) {
+				if( n1.val < n2.val){
+					return -1;
+				}else{
+					if( n1.val > n2.val)
+						return 1;
+					else
+						return 0;
+				}
+
+			}
+		};
+		PriorityQueue<ListNode> queue = new PriorityQueue<ListNode>(comp);
+
+		ListNode newHead = null;
+		ListNode currIter=newHead;
+		for(ListNode currNode : A){
+			if( currNode != null){
+				queue.offer( currNode);
+			}
+		}
+		while(!queue.isEmpty()){
+			ListNode removedNode = queue.poll();
+			if( newHead == null){
+				newHead = removedNode;
+				currIter = newHead;
+			}else{
+				currIter.next= removedNode;
+				currIter=removedNode;
+			}
+			if( removedNode.next != null){
+				queue.offer( removedNode.next);
+			}
+		}
+		return newHead;
+	}
+}
